ed034ec9855a7b71145b02687e859a7f5fde34d1
ferasalnaem <feras_alnaem@outlook.com>
2019-12-03 15:42:08 +0100

    adding prediction

--- plot_history.py
+++ plot_history.py
@@ -4,24 +4,25 @@ import argparse
 import os
 
 
-def get_args():
-    parser = argparse.ArgumentParser(description="Plots training curves from history file.")
-    parser.add_argument("--input", "-i", type=str, required=True,
-                        help="path to history h5 file")
-    args = parser.parse_args()
-    return args
+#def get_args():
+#    parser = argparse.ArgumentParser(description="Plots training curves from history file.")
+#    parser.add_argument("--input", "-i", type=str, required=True,
+#                        help="path to history h5 file")
+#    args = parser.parse_args()
+#    return args
 
 
 def main():
-    args = get_args()
-    input_path = args.input
+#    args = get_args()
+#    input_path = args.input
+    input_path='/home/feras/GenderDetector/models/history.h5'
 
     df = pd.read_hdf(input_path, "history")
     input_dir = os.path.dirname(input_path)
     plt.plot(df["gender_loss"], label="loss (gender)")
-    plt.plot(df["age_loss"], label="loss (age)")
+    #plt.plot(df["age_loss"], label="loss (age)")
     plt.plot(df["val_gender_loss"], label="val_loss (gender)")
-    plt.plot(df["val_age_loss"], label="val_loss (age)")
+    p#lt.plot(df["val_age_loss"], label="val_loss (age)")
     plt.xlabel("number of epochs")
     plt.ylabel("loss")
     plt.legend()
@@ -29,9 +30,9 @@ def main():
     plt.cla()
 
     plt.plot(df["gender_acc"], label="accuracy (gender)")
-    plt.plot(df["age_acc"], label="accuracy (age)")
+    #plt.plot(df["age_acc"], label="accuracy (age)")
     plt.plot(df["val_gender_acc"], label="val_accuracy (gender)")
-    plt.plot(df["val_age_acc"], label="val_accuracy (age)")
+    #plt.plot(df["val_age_acc"], label="val_accuracy (age)")
     plt.xlabel("number of epochs")
     plt.ylabel("accuracy")
     plt.legend()
--- predict.py
+++ predict.py
@@ -39,7 +39,7 @@ def detect_faces(img):
 
 
 model = MobileNetDeepEstimator(IMG_SIZE, 1, 21, weights=None)()
-model.load_weights(os.path.join("checkpoints", "weights.62-1.97.hdf5"))
+model.load_weights(os.path.join("checkpoints", "weights.01-0.20.hdf5"))
 
 base_path = "test"
 
